0c3ca0890e65bd49c8041c5c7f60a8469e57c324
evanmac <evanmac@udel.edu>
2020-03-07 18:33:04 -0500

    Wikipedia Scraper Complete, Trigram Language Model Complete

--- hw_1b.py
+++ hw_1b.py
@@ -1,44 +1,23 @@
 
-ASCII_OFFSET = 97
-
-
-'''
-def add_to_char_list(char_list, char, num_appearances):
-    char_list[ord(char) - ASCII_OFFSET] += num_appearances
-    return char_list
-'''
-
-'''
-def init_bigram_dict():
-    bigrams = {}
-    for i in range(0, 26):
-        bigrams[chr(i + ASCII_OFFSET)] = init_next_char_list()
-    return bigrams
-
-
-def init_trigram_dict():
-    trigrams = {}
-    for i in range(0, 26):
-        for j in range(0, 26):
-            key = chr(i + ASCII_OFFSET) + chr(j + ASCII_OFFSET)
-            trigrams[key] = init_next_char_list()
-    return trigrams
-
-
-print(init_bigram_dict())
-print("")
-print(init_trigram_dict())
-'''
-
-data_pairs = []
+training_data = []
 
 with open("training set.txt", "r") as file:
     for line in file:
-        data = line.strip().split('\t')
-        data_pairs.append([data[0].strip(), int(data[1])])
+        data = line.split('\t')
+        training_data.append([" " +data[0].strip() + " ", int(data[1])])
 
 
-def init_n_gram_dict():
+def init_n_gram_dict(data_pairs):
+    '''
+    Initializes:
+    char_count, a total count of all the characters
+    monogram_counts, dictionary where monogram_counts[a] is the number of appearances of char 'a'
+    bigram_counts, dictionary where bigram_counts[a][b] is the number of appearances of substring 'ab'
+    trigram_counts, dictionary where trigram_counts['ab'][c] is the number of appearances of substring 'abc'
+    '''
+
+    char_count = 0
+    monogram_counts = {}
     bigram_counts = {}
     trigram_counts = {}
 
@@ -48,53 +27,167 @@ def init_n_gram_dict():
             next_char = data[0][i + 1]
             if char not in bigram_counts:
                 bigram_counts[char] = {}
-                bigram_counts[char][next_char] = data[1]
+                bigram_counts[char][next_char] = [data[1]]
             elif next_char not in bigram_counts[char]:
-                bigram_counts[char][next_char] = data[1]
+                bigram_counts[char][next_char] = [data[1]]
             else:
-                bigram_counts[char][next_char] += data[1]
+                bigram_counts[char][next_char][0] += data[1]
 
-        for i in range(1, len(data[0]) - 2):    #This range might be incorrect
+        for i in range(1, len(data[0]) - 2):
             bigram_key = data[0][i - 1: i + 1]
             next_char = data[0][i + 1]
             if bigram_key not in trigram_counts:
                 trigram_counts[bigram_key] = {}
-                trigram_counts[bigram_key][next_char] = data[1]
+                trigram_counts[bigram_key][next_char] = [data[1]]
             elif next_char not in trigram_counts[bigram_key]:
-                trigram_counts[bigram_key][next_char] = data[1]
+                trigram_counts[bigram_key][next_char] = [data[1]]
             else:
-                trigram_counts[bigram_key][next_char] += data[1]
-    return bigram_counts, trigram_counts
+                trigram_counts[bigram_key][next_char][0] += data[1]
+
+        for i in range(0,len(data[0])):
+            mono_gram_key = data[0][i]
+            char_count += data[1]
+            if mono_gram_key not in monogram_counts:
+                monogram_counts[mono_gram_key] = [data[1]]
+            else:
+                monogram_counts[mono_gram_key][0] += data[1]
+    return char_count, monogram_counts, bigram_counts, trigram_counts
 
 
-bigram_counts, trigram_counts = init_n_gram_dict()
 
 
-def predict_next_char(prev_chars, n_gram_dict):
-    return rank_most_likely_char(n_gram_dict[prev_chars])
+
+def compute_prob_dict(monogram_cts, bigram_cts, trigram_cts, char_ct):
+    '''
+    Updates the count dictionaries with the probability of the given event occuring
+    p_char_given_bigram['ab']['c'][1] is the probability of seeing c given 'ab' was before
+    p_char_given_monogram['a']['b][1] is the probablility of seeing b given a was before
+    p_char['a'] is the probability of seeing 'a'
+    '''
+
+    p_char_given_bigram = trigram_cts
+    p_char_given_monogram = bigram_cts
+    p_char = monogram_cts
+    char_count = char_ct
+
+    for char in p_char:
+        p_char[char].append(p_char[char][0] / char_count)
+
+    for monogram_key in p_char_given_monogram:
+        for char in p_char_given_monogram[monogram_key]:
+            p_char_given_monogram[monogram_key][char].append(p_char_given_monogram[monogram_key][char][0] /  monogram_counts[monogram_key][0] )
+    for bigram_key in p_char_given_bigram:
+        for char in p_char_given_bigram[bigram_key]:
+            p_char_given_bigram[bigram_key][char].append( p_char_given_bigram[bigram_key][char][0]/bigram_counts[bigram_key[0]][bigram_key[1]][0])
+    return p_char, p_char_given_monogram, p_char_given_bigram
 
 
-def rank_most_likely_char(adict):
+def predict_next_char(prev_chars):
+    '''
+    Ranks the next possible chars given that prev_chars has been seen.
+    Returns a list of lists in the form [char, count, probability] where
+    the most likely next char appears first
+    '''
     letter_counts = []
-    for key in adict:
-
-        temp = [str(key), int(adict[key])]
-        letter_counts.append(temp)
-    ranking = sorted(letter_counts, key=lambda x: x[1],reverse=True)
+    if len(prev_chars) == 1:
+        if prev_chars in bigram_counts:
+            for key in bigram_counts[prev_chars]:
+                temp = [str(key), int(bigram_counts[prev_chars][key][0]), float(bigram_counts[prev_chars][key][1])]
+                letter_counts.append(temp)
+    elif len(prev_chars) == 2:
+        if prev_chars in trigram_counts:
+            for key in trigram_counts[prev_chars]:
+                temp = [str(key), int(trigram_counts[prev_chars][key][0]), float(trigram_counts[prev_chars][key][1])]
+                letter_counts.append(temp)
+    ranking = sorted(letter_counts, key=lambda x: x[2], reverse=True)
     return ranking
 
-print(predict_next_char("ab", trigram_counts))
 
 
 
 
+def interactive_next_char_predictor():
+    '''
+    Creates an interactive prompt. User inputs characters, most likely next characters are printed and
+    the rank of the observed character is printed
+    '''
+    input_string = ""
+    char = input("Input a single character, or nothing to exit")
+    if not char:
+        return
+    next_char_ranking = predict_next_char(char)
+    input_string += char
+    print("Most likely next characters:")
+    print("1. \'" + next_char_ranking[0][0] + "\' (%" + str(round(next_char_ranking[0][2] * 100, 3)) + ")")
+    print("2. \'" + next_char_ranking[1][0] + "\' (%" + str(round(next_char_ranking[1][2] * 100, 3)) + ")")
+    print("3. \'" + next_char_ranking[2][0] + "\' (%" + str(round(next_char_ranking[2][2] * 100, 3)) + ")")
+    chars_before = char
+    char = input("Input a single character, or nothing to exit")
+    if not char:
+        return
+    input_string += char
+
+    while char:
+        observed_prob = calc_observed_probability(char, chars_before)
+        observed_rank = find_observed_rank(next_char_ranking, char, observed_prob)
+        print("The observed character \'" + char + "\' was ranked " + str(observed_rank) + " most likely (%" + str(
+            round(observed_prob * 100, 3)))
+        chars_before = chars_before[-1] + char
+        next_char_ranking = predict_next_char(chars_before)
+        print("Most likely next characters:")
+        if next_char_ranking:
+            i = 0
+            while i < 3 and i < len(next_char_ranking):
+                print(str(i + 1) + ". \'" + next_char_ranking[i][0] + "\' (%" + str(round(
+                    next_char_ranking[i][2] * 100, 3)) + ")")
+                i += 1
+
+        print("Your String: " + input_string)
+
+        char = input("Input a single character, or nothing to exit")
+        input_string += char
 
 
 
+def find_observed_rank(rank_list, observed_char, p):
+    '''
+    Finds the rank of the observed char given previous chars.
+
+    '''
+    for i in range(0, len(rank_list)):
+        if rank_list[i][0] == observed_char:
+            return i + 1
+    #If observed_char is not in rank list, use stupid backoff probability
+    rank_list.append([observed_char, -1, p])
+    updated_ranking = sorted(rank_list, key=lambda x: x[2], reverse=True)
+    for i in range(0, len(updated_ranking)):
+        if updated_ranking[i][0] == observed_char:
+            return i + 1
+
+
+def calc_observed_probability(observed, previous_chars):
+    '''
+    Calculates the probability of seeing observed given previous_chars
+    If P(observed | previous_chars) = 0, Use stupid backoff
+    '''
+    if len(previous_chars) == 1:
+        if previous_chars in p_char_given_monogram and observed in p_char_given_monogram[previous_chars]:
+            return p_char_given_monogram[previous_chars][observed][-1]
+        else:
+            return .4 * p_char[observed] #Applying stupid backoff
+    if len(previous_chars) == 2:
+        if previous_chars in p_char_given_bigram and observed in p_char_given_bigram[previous_chars]:
+            return p_char_given_bigram[previous_chars][observed][-1]
+        elif observed in p_char_given_monogram[previous_chars[-1]]:
+            return p_char_given_monogram[previous_chars[-1]][observed][-1] * .4 #Stupid backoff
+        else:
+            return .16 * p_char[observed] #Stupid backoff twice
 
 
 
-
+char_count, monogram_counts, bigram_counts, trigram_counts = init_n_gram_dict(training_data)
+p_char, p_char_given_monogram, p_char_given_bigram = compute_prob_dict(monogram_counts, bigram_counts, trigram_counts, char_count)
+interactive_next_char_predictor()
 
 
 
--- hw_1c.py
+++ hw_1c.py
@@ -1,21 +1,41 @@
 import wikipedia
 import re
+from bs4 import BeautifulSoup
 
+state_names = ["California", "Nevada", "Arizona", "Florida", "Maryland", "Pennsylvania",
+               "New Jersey", "North Carolina", "Vermont", "Idaho"]
 
 def lookup_state_wiki(state_name):
-    page = wikipedia.page(state_name)
-    if page:
-        return page.content
+    '''
+    Finds wikipedia page for given state_name
+    Converts html to text
+    '''
+    page_html = wikipedia.WikipediaPage(state_name).html()
+    if page_html:
+        soup = BeautifulSoup(page_html, "html.parser")
+        return soup.get_text()
+    else:
+        print("State not found")
+
 
 
 def lookup_GSP(wiki_text):
-    regex = '(?:G|g)ross state product[A-Za-z\(\) 0-9]*\$([0-9]*\.?[0-9]* (?:billion|trillion|million))'
+    '''
+    Takes text from a wikipedia page
+    Uses a regex to find the state's gross state product
+    '''
+    regex = '(?:G|g)ross state product[A-Za-z\(\)\s0-9]*\$([0-9]*\.?[0-9]*\s(?:billion|trillion|million))'
     match = re.search(regex, wiki_text)
     if match:
         return match.groups()[0]
+    else:
+        return "Not Found"
 
 
 def convert_GSP_to_int(GSP):
+    '''
+    Converts GSP found with lookup_GSP into a number
+    '''
     amt, unit = tuple(GSP.split())
     if unit == 'billion':
         return int(float(amt) * 1000000000)
@@ -25,21 +45,59 @@ def convert_GSP_to_int(GSP):
         return int(float(amt) * 1000000)
 
 
-cali_text = lookup_state_wiki("California")
-GSP = lookup_GSP(cali_text)
-GSP_int = convert_GSP_to_int(GSP)
-print(GSP_int)
-
-penn_text = lookup_state_wiki("Pensylvania")
-print(penn_text)
-
 def look_up_nicknames(wiki_text):
-    regex = 'Nickname\('
+    '''
+    Finds the states nicknames
+    '''
+    regex = r'Nickname\(s\)\:\s([A-za-z;\s0-9\[\]\(\)\",]+)Motto'
     match = re.search(regex, wiki_text)
     if match:
-        return match.group(0)
+        return match.groups()[0]
+    else:
+        return "Not Found"
 
-print(look_up_nicknames(penn_text))
 
-#\(s\): ([A-Za-z]+);
+def clean_nickname_data(data):
+    '''
+    Cleans the nicknames retrieved by look_up_nicknames
+    '''
+    data = re.sub("\[[0-9]\]", "", data)
+    data = data.replace("\"", "")
+    temp = re.split(r'[;,]', data)
+   #temp = data.split(";")
+    nicknames = []
+    for nickname in temp:
+        nicknames.append(nickname.strip())
+    return nicknames
+
+
+
+
+
+def collect_state_info(state_names):
+    """
+    Iterates through given state_names
+    Finds the states GSP and nicknames
+    stores this data in a file called output.tsv
+    """
+    state_outputs = []
+    for state in state_names:
+        state_text = lookup_state_wiki(state)
+        GSP = lookup_GSP(state_text)
+        if GSP != "Not Found":
+            GSP = convert_GSP_to_int(GSP)
+        nicknames = look_up_nicknames(state_text)
+        nicknames = clean_nickname_data(nicknames)
+        nicknames_as_string = ";"
+        nicknames_as_string = nicknames_as_string.join(nicknames)
+        state_outputs.append(state + '\t' + str(GSP) + '\t' + nicknames_as_string)
+    with open('output.tsv', 'w') as fp:
+        for state_output in state_outputs:
+            fp.write(state_output + '\n')
+
+
+collect_state_info(state_names)
+
+
+
 
